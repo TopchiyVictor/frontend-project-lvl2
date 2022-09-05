551a442ace6808c29e246fd924df53d749f43894
Topchiy Victor <krasilnikof@yandex.ru>
2022-09-05 17:44:58 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 const program = new Command();
 program
 .version('0.0.1')
@@ -8,4 +9,7 @@ program
 .argument('<filepath1>')
 .argument('filepath2')
 .option('-f, --format <type>', 'output format')
+.action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2));
+});
 program.parse();
