e63faeec901bd176e6aa03f0ff09a8d758a62e62
Topchiy Victor <krasilnikof@yandex.ru>
2022-09-13 23:53:03 +0300

    fix commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,15 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
+
 const program = new Command();
 program
-.version('0.0.1')
-.description('Compares two configuration files and shows a difference.')
-.argument('<filepath1>')
-.argument('<filepath2>')
-.option('-f, --format <type>', 'output format')
-.action((filepath1, filepath2) => {
-     console.log(genDiff(filepath1, filepath2));
-});
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();
