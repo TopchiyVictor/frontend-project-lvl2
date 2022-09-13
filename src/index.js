3415ac221c67e5cd1c789183a48846f7766e360d
Topchiy Victor <krasilnikof@yandex.ru>
2022-09-13 23:56:42 +0300

    fix bugs and rules eslint

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
- import _ from 'lodash';
- import path from 'path';
- import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs';
 
 const getDiffInfo = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const uniq = _.uniq([...keys1, ...keys2]);
   const sortUniq = _.sortBy(uniq);
-  const result  = sortUniq.map((key) => {
+  const result = sortUniq.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    if (_.isEqual(value1,value2)) {
-      return {
-        type: 'unchanged',
-        key,
-        value: value1,
-      };
-    }
     if ((value1 && value2) && (value1 !== value2)) {
       return {
         type: 'changed',
         key,
         value1,
         value2,
-      }
+      };
     }
-    if (!_.has(obj2,key)) {
+    if (!_.has(obj2, key)) {
       return {
         type: 'delited',
         key,
         value: value1,
-      }
+      };
     }
-    if (!_.has(obj1,key)) {
+    if (!_.has(obj1, key)) {
       return {
         type: 'added',
         key,
-        value: value2
-      }
+        value: value2,
+      };
     }
-    });
+    return {
+      type: 'unchanged',
+      key,
+      value: value1,
+    };
+  });
   return result;
 };
 
@@ -55,11 +53,13 @@ const getDiff = (diffInfo) => {
         return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
       case 'added':
         return `  + ${diff.key}: ${diff.value}`;
+      default:
+        return null;
     }
-  })
-  return `{\n${result.join('\n')}\n}`
-}
-const getAbsolutPath = (filePath) => path.resolve(process.cwd(),'files', filePath);
+  });
+  return `{\n${result.join('\n')}\n}`;
+};
+const getAbsolutPath = (filePath) => path.resolve(process.cwd(), 'files', filePath);
 
 const readFile = (filePath) => fs.readFileSync(getAbsolutPath(filePath), 'utf-8');
 
